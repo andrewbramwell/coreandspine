fcec8ba4d22f732a316bc099c7c7106c4b52ca6e
andrewbramwell <97716059+andrewbramwell@users.noreply.github.com>
2025-06-28 10:53:14 +0100

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "name": "Chokdee Thai Therapy",
+    "email": "chokdeethaitherapy@gmail.com",
+    "phoneForTel": "+447958229813",
+    "phoneFormatted": "07958 229813",
+    "openingHours": "Mon-Fri: 10am-7pm, Sat: 3.30pm-7pm",
+    "onlineBooking": "https://chokdeethaitherapy.setmore.com/",
+    "address": {
+        "lineOne": "77 Queen St",
+        "lineTwo": "",
+        "city": "Normanton",
+        "state": "West Yorkshire",
+        "zip": "WF6 1AF",
+        "country": "United Kingdom",
+        "mapLink": "https://maps.app.goo.gl/866FWxeEERejDbR8A"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+        "googleReview": "https://g.page/r/Carswx-vZttxEBM/review",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.example.com"
+};
